6636dd6cc6e7d45a3cf2b338c741a4bccb325291
Dmytro Karpyuk <52283766+DmytroKarpyuk@users.noreply.github.com>
2020-08-10 01:46:55 +0200

    Update Cart.js

--- models/Cart.js
+++ models/Cart.js
@@ -6,13 +6,31 @@ module.exports = function Cart(oldCart) {
     this.add = (item, id) => {
         let storedItem = this.items[id]
         if (!storedItem) {
-            storedItem = this.items[id] = { item: item, qty: 0, price: 0 }
+            storedItem = this.items[id] = {item: item, qty: 0, price: 0}
         }
         storedItem.qty++
         storedItem.price = storedItem.item.price * storedItem.qty
         this.totalQty++
         this.totalPrice += storedItem.item.price
     }
+
+    this.reduceByOne = (id) => {
+        this.items[id].qty--
+        this.items[id].price -= this.items[id].item.price
+        this.totalQty--
+        this.totalPrice -= this.items[id].item.price
+        if (this.items[id].qty <= 0) {
+            delete this.items[id]
+        }
+    }
+
+    this.removeItem = (id) => {
+        this.totalQty -= this.items[id].qty
+        this.totalPrice -= this.items[id].price
+        delete this.items[id]
+    }
+
+
 
     this.generateArray = () => {
         let arr = []
@@ -22,35 +40,3 @@ module.exports = function Cart(oldCart) {
         return arr
     }
 }
-
-
-
-// const { Schema, model } = require('mongoose')
-
-// const cart = new Schema(
-//     {
-//         userId: {
-//             type: Schema.Types.ObjectId,
-//             ref: "User"
-//         },
-//         products: [
-//             {
-//                 productId: Number,
-//                 quantity: Number,
-//                 name: String,
-//                 price: Number
-//             }
-//         ],
-//         active: {
-//             type: Boolean,
-//             default: true
-//         },
-//         modifiedOn: {
-//             type: Date,
-//             default: Date.now
-//         }
-//     },
-//     { timestamps: true }
-// );
-
-// module.exports = model('Cart', cart)
